da3cab44b44011c80020e4dbf21e64c149f08f77
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-01-30 20:34:15 -0500

    Configuración de la página inicial de la aplicación

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -10,14 +10,14 @@ export const Hero = () => {
             <div className="hero-left">
                 <h2>NEW ARRIVALS ONLY</h2>
                 <div>
-                    <div className="hand-hand-icon">
+                    <div className="hero-hand-icon">
                         <p>new</p>
                         <img src={hand_icon} alt="" />
                     </div>
                     <p>collections</p>
                     <p>for everyone</p>
                 </div>
-                <div className="hero-latest-bin">
+                <div className="hero-latest-btn">
                     <div>Latest Collection</div>
                     <img src={arrow_icon} alt="" />
                 </div>
